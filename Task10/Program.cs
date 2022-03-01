21411c908105c2fd22f1de5f4559e4a7c2f1f4e5
Victor <peticavictor@rambler.ru>
2022-03-01 22:52:05 +0200

    Some changes

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -10,17 +10,17 @@ namespace Australia
 
 namespace Canada
 {
-    class Sydney
+    class Ottawa
     {
-        public static int population = 29_904;
+        public static int population = 994_837;
     }
 }
 
 namespace USA
 {
-    class Miami
+    class WashingtonDC
     {
-        public static int population = 454_279;
+        public static int population = 692_683;
     }
 }
 
@@ -30,9 +30,9 @@ namespace Program
     {
         public static void Main(String[] args)
         {
-            Console.WriteLine(Australia.Sydney.population > Canada.Sydney.population ? "Australian Sydney is bigger" : "Canadian Sydney is bigger");
-            Console.WriteLine(USA.Miami.population > Canada.Sydney.population ? "Miami is bigger" : "Canadian Sydney is bigger");
-            Console.WriteLine(USA.Miami.population > Australia.Sydney.population ? "Miami is bigger" : "Australian Sydney is bigger");
+            Console.WriteLine(Australia.Sydney.population > Canada.Ottawa.population ? "Australian Sydney is bigger" : "Canadian Ottawa is bigger");
+            Console.WriteLine(USA.WashingtonDC.population > Canada.Ottawa.population ? "Washington DC is bigger" : "Canadian Ottawa is bigger");
+            Console.WriteLine(USA.WashingtonDC.population > Australia.Sydney.population ? "Washington DC is bigger" : "Australian Sydney is bigger");
         }
     }
 }
